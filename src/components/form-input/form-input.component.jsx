bbb4f638302f641c79bec491b35f248a516cd4c4
piklume <76256255+piklume@users.noreply.github.com>
2021-06-01 01:20:39 +0530

    Finish signin and submit method

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 import './form-input.styles.scss';
 
-const FormInput = ({handelChange, label, ...otherProps}) => (
+const FormInput = ({handleChange, label, ...otherProps}) => (
     <div className="group">
-        <input className='form-input' onChange={handelChange} {...otherProps} />
+        <input className='form-input' onChange={handleChange} {...otherProps} />
         {
             label ?
             (<label className={`${otherProps.value.length? 'shrink': ''} form-input-label`}>
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import FormInput from '../form-input/form-input.component.jsx';
 import CustomButtom from '../custom-button/custom-button.component.jsx';
 
-import { signInWithGoogle } from '../../firebase/firebase.utils.js';
+import { auth,signInWithGoogle } from '../../firebase/firebase.utils.js';
 
 import './sign-in.styles.scss';
 
@@ -17,17 +17,24 @@ class SignIn extends React.Component {
         }
     }
 
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault();
 
-        this.setState({email: '', password: ''});
-    }
+        const { email, password } = this.state;
+
+        try {
+            await auth.signInWithEmailAndPassword(email,password);
+            this.setState({ email: '', password: ''});
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
     handleChange = (event) => {
         const {value, name} = event.target;
 
         this.setState({[name]: value});
-    }
+    };
 
     render() {
         return(
