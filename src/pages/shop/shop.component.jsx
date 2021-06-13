0735282f472dd094ecf5f0f282359ac8db7da0e8
piklume <76256255+piklume@users.noreply.github.com>
2021-06-13 23:21:32 +0530

    did a lot of stuff, each category has distinct landing pages now

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,30 +1,16 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 
-import SHOP_DATA from './shop.data.js';
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+import CollectionsOverview from '../../components/collections-overview/collections-overview.components';
+import CollectionPage from '../collection/collection.component';
 
-class ShopPage extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state ={
-            collections: SHOP_DATA
-        }
-    }
-    
-    render() {
-        const {collections} = this.state;
-        return (<div className='shop-page'>
-            {
-                collections.map(({id,...otherCollectionProps})  => (
-                <CollectionPreview key={id} {...otherCollectionProps}/> 
-                    )
-                )
-            }
-        </div>
-
-        )
-    }
-}
+const ShopPage = ({ match }) => {
+    return (
+    <div className='shop-page'>
+        <Route exact path={`${match.path}`} component={CollectionsOverview} />
+        <Route path={`${match.path}/:collectionid`} component={CollectionPage} />
+    </div>
+    )
+};
 
 export default ShopPage;
