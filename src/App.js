83cf97d8db1f0a095b0369aea99ac7b57f6d76cb
piklume <76256255+piklume@users.noreply.github.com>
2021-06-15 23:20:33 +0530

    Added my contact info and created the contact page for that

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
 import Header from './components/header/header.component.jsx';
 import CheckoutPage from './pages/checkout/checkout.component.jsx';
+import Contact from './pages/contact/contact.component';
 
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx';
 import {auth,createUserProfileDocument} from './firebase/firebase.utils';
@@ -51,6 +52,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
+          <Route path='/contact' component={Contact} />
           <Route exact path='/checkout' component={CheckoutPage} />
           <Route exact path='/signin'  render={() => this.props.currentUser ? (<Redirect to='.' />):(<SignInAndSignUpPage />)}  />
         </Switch>
---
+++ src/pages/contact/contact.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './contact.styles.scss';
+
+const Contact = () => (
+    <div className='contact'>
+        <h1>Project by Sayak Dutta</h1>
+        <h3><a href='https://sayakdutta.netlify.app/' target="_blank" rel = "noopener noreferrer">
+            Click here to contact me!
+        </a></h3>
+    </div>
+);
+
+export default Contact;
