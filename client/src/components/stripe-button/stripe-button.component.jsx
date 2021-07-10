0fd2d313e34fcfb043105adb5d0d3d07fd44ec19
piklume <76256255+piklume@users.noreply.github.com>
2021-07-10 14:19:12 +0530

    Remove gitignore files

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
+
 
 
 const StripeCheckoutButton = ({ price }) => {
@@ -7,8 +9,25 @@ const StripeCheckoutButton = ({ price }) => {
     const publishableKey = 'pk_test_51J2fJxSGuqgKrT0XvNC1ShTQenbwhLDmuXUCC5SVAsrVcHDaA5wsotCVolVPH8rZ1NE1NxyhndiNJVCNw28yHZ8n0025qn0pgt';
 
     const onToken = token => {
-        console.log(token);
-        alert('Payment Successful!');
+        // console.log(token);
+        // alert("Payment done!");
+       axios({
+           url: 'payment',
+           method: 'post',
+           data: {
+               amount: priceForStripe,
+               token
+           }
+       }).then(response => {
+           console.log(response);
+           alert('Payment Successful');
+       }).catch(error => {
+        console.log(error)
+           console.log('Payment error: ', JSON.parse(error));
+           alert(
+               'There was an issue with your payment. Please use the provided credit card'
+           );
+       });
     }
 
     return (
